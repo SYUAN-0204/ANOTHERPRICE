b1e9ea1d0437f5c75542fcc7c534a6f0311eaea9
SYUAN <wangsyuanyun20030204@gmail.com>
2025-04-21 01:38:01 +0800

    temp

--- ANOTHERPRICE/temp3.swift
+++ ANOTHERPRICE/temp3.swift
@@ -12,6 +12,8 @@ struct temp3: View {
     
     @State private var 匿名 = false
     @State var userAvatar: UIImage = UIImage(named: "Logo_122D3E") ?? UIImage()
+    @State private var 自己提問: Bool = false
+    @State private var 關注: Bool = false
     @State private var tags = "#標籤 #不同標籤"
     @State private var title: String = "標題"
     @State private var like: Bool = false
@@ -22,19 +24,21 @@ struct temp3: View {
     @State private var code: String = "/*edwefwec8*/"
     @State private var http: String = "https://www.anotherprice.com"
     @State private var 排序方式: Bool = false
-    @State private var input: String = ""
+    @State private var 回答: String = ""
+    @State private var 評論: String = "一條很長長長長e長的評論，一條很長長長長長的評論，一條很長長長長長的評論，一條很長長長長長的評論，一條很長長長長長的評論"
+    @State private var 展開狀態: Bool = false
     
     var body: some View {
         VStack(alignment: .leading){
             HStack {
                 HStack{
-                        Button {
-                            dismiss()
-                        } label: {
-                            Image(systemName: "arrow.left")
-                                .font(.system(size: 18))
-                                .foregroundColor(.black)
-                        }
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "arrow.left")
+                            .font(.system(size: 18))
+                            .foregroundColor(.black)
+                    }
                     Spacer()
                 }
                 .padding(.leading, 10)
@@ -55,41 +59,57 @@ struct temp3: View {
                 NavigationLink{
                     tempView()
                 } label: {
-                        Image(uiImage: userAvatar)
-                            .resizable()
-                            .scaledToFill() // 確保填滿圓形
-                            .frame(width: 40, height: 40) // 限制大小
-                            .background(Color(.systemGray6))
-                            .clipShape(Circle()) // 剪裁為圓形
-                            .overlay(
-                                Circle().stroke(ColorConstants.systemMainColor, lineWidth: 1)
-                            )
-                        Text(匿名 ? "匿名精靈":"用戶名稱")
-                            .font(.custom("LXGWWenKaiMonoTC-Regular", size: 17))
-                            .foregroundColor(ColorConstants.systemSubColor)
+                    Image(uiImage: userAvatar)
+                        .resizable()
+                        .scaledToFill() // 確保填滿圓形
+                        .frame(width: 40, height: 40) // 限制大小
+                        .background(Color(.systemGray6))
+                        .clipShape(Circle()) // 剪裁為圓形
+                        .overlay(
+                            Circle().stroke(ColorConstants.systemMainColor, lineWidth: 1)
+                        )
+                    Text(匿名 ? "匿名精靈":"用戶名稱")
+                        .font(.custom("LXGWWenKaiMonoTC-Regular", size: 17))
+                        .foregroundColor(ColorConstants.systemSubColor)
                 }
                 .disabled(匿名)
                 if !匿名 {
                     UITextLevel(totalExp: 3564)
                     Spacer()
-                    Button{
-                    } label: {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 3)
-                                .stroke(ColorConstants.systemMainColor, lineWidth: 1)
-                                .frame(width: 66, height: 24)
-                            HStack{
-                                Image(systemName: "plus")
-                                    .font(.system(size: 12))
-                                    .foregroundColor(ColorConstants.systemMainColor)
-                                    .padding(.trailing, -7)
-                                Text("關注")
-                                    .font(.custom("LXGWWenKaiMonoTC-Regular", size: 16))
-                                    .foregroundColor(ColorConstants.systemMainColor)
+                    if !自己提問 {
+                        Button{
+                            關注.toggle()
+                        } label: {
+                            if 關注 {
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 3)
+                                        .fill(ColorConstants.systemMainColor)
+                                        .frame(width: 66, height: 24)
+                                    HStack{
+                                        Text("已關注")
+                                            .font(.custom("LXGWWenKaiMonoTC-Regular", size: 16))
+                                            .foregroundColor(.white)
+                                    }
+                                }
+                            }
+                            else {
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 3)
+                                        .stroke(ColorConstants.systemMainColor, lineWidth: 1)
+                                        .frame(width: 66, height: 24)
+                                    HStack{
+                                        Image(systemName: "plus")
+                                            .font(.system(size: 12))
+                                            .foregroundColor(ColorConstants.systemMainColor)
+                                            .padding(.trailing, -7)
+                                        Text("關注")
+                                            .font(.custom("LXGWWenKaiMonoTC-Regular", size: 16))
+                                            .foregroundColor(ColorConstants.systemMainColor)
+                                    }
+                                }
                             }
                         }
                     }
-                    .disabled(true)
                 }
             }
             .padding(.horizontal, 15)
@@ -97,7 +117,7 @@ struct temp3: View {
                 .fill(.gray.opacity(0.3))
                 .frame(height: 1)
                 .padding(.horizontal, 10)
-            ScrollView{
+            ScrollView(showsIndicators: false){
                 VStack(alignment: .leading){
                     HStack{
                         Text("title")
@@ -114,13 +134,13 @@ struct temp3: View {
                         }
                     }
                     HStack{
-                            Text("科技")
-                                .font(.custom("LXGWWenKaiMonoTC-Regular", size: 14))
-                                .foregroundColor(ColorConstants.systemDarkColor)
-                                .lineLimit(1)
-                                .padding(.horizontal, 3)
-                                .background(ColorConstants.systemMainColor.opacity(0.2))
-                                .cornerRadius(3)
+                        Text("科技")
+                            .font(.custom("LXGWWenKaiMonoTC-Regular", size: 14))
+                            .foregroundColor(ColorConstants.systemDarkColor)
+                            .lineLimit(1)
+                            .padding(.horizontal, 3)
+                            .background(ColorConstants.systemMainColor.opacity(0.2))
+                            .cornerRadius(3)
                         Text("Deadline : 2025-05-23")
                             .font(.custom("LXGWWenKaiMonoTC-Regular", size: 14))
                             .foregroundColor(.gray)
@@ -204,12 +224,82 @@ struct temp3: View {
                     }
                 }
                 .padding(.horizontal, 5)
-                Spacer()
+                ForEach(0..<5) { _ in
+                    NavigationLink{
+                        tempView()
+                    } label: {
+                        HStack{
+                            Image(uiImage: userAvatar)
+                                .resizable()
+                                .scaledToFill() // 確保填滿圓形
+                                .frame(width: 30, height: 30) // 限制大小
+                                .background(Color(.systemGray6))
+                                .clipShape(Circle()) // 剪裁為圓形
+                                .overlay(
+                                    Circle().stroke(ColorConstants.systemMainColor, lineWidth: 1)
+                                )
+                            VStack(alignment: .leading){
+                                Text(匿名 ? "神秘旅人":"用戶名稱")
+                                    .font(.custom("LXGWWenKaiMonoTC-Regular", size: 15))
+                                    .foregroundColor(ColorConstants.systemSubColor)
+                                Text("2025-04-13 16:32")
+                                    .font(.custom("LXGWWenKaiMonoTC-Regular", size: 10))
+                                    .foregroundColor(.gray)
+                            }
+                            Spacer()
+                            Button{
+                                
+                            } label: {
+                                Image(systemName: "ellipsis")
+                                    .font(.system(size: 12))
+                                    .foregroundColor(.gray)
+                                    .frame(width: 24, height: 24, alignment: .trailing)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 5)
+                    VStack(alignment: .leading){
+                        Text(評論)
+                            .font(.custom("LXGWWenKaiMonoTC-Regular", size: 17))
+                            .foregroundColor(ColorConstants.systemDarkColor)
+                            .lineLimit(展開狀態 ? nil:3)
+                        if 評論.count > 50 {
+                            Button(action: {
+                                展開狀態.toggle()
+                            }) {
+                                Text(展開狀態 ? "收起" : "展開")
+                                    .font(.custom("LXGWWenKaiMonoTC-Regular", size: 15))
+                                    .foregroundColor(ColorConstants.systemMainColor)
+                            }
+                        }
+                        HStack{
+                            Spacer()
+                            Button{
+                                like.toggle()
+                            } label: {
+                                Image(systemName: like ? "heart.fill":"heart")
+                                    .font(.system(size: 14))
+                                    .foregroundColor(ColorConstants.systemMainColor)
+                            }
+                            Text("\(heart)")
+                                .font(.custom("LXGWWenKaiMonoTC-Regular", size: 14))
+                                .foregroundColor(ColorConstants.systemMainColor)
+                        }
+                        .padding(.trailing, 8)
+                    }
+                    .padding(.leading, 38)
+                    .padding(.horizontal, 5)
+                    Rectangle()
+                        .fill(.gray.opacity(0.3))
+                        .frame(height: 1)
+                        .padding(.bottom, 5)
+                }
             }
             .padding(.horizontal, 10)
             HStack{
                 HStack{
-                    TextField("" ,text: $input)
+                    TextField("" ,text: $回答)
                         .autocapitalization(.none)
                         .font(.custom("LXGWWenKaiMonoTC-Regular", size: 18))
                         .foregroundColor(ColorConstants.systemSubColor)
@@ -235,10 +325,10 @@ struct temp3: View {
                 } label: {
                     ZStack{
                         RoundedRectangle(cornerRadius: 5)
-                            .fill(ColorConstants.systemMainColor.opacity(input.isEmpty ? 0.7 : 1.0))
+                            .fill(ColorConstants.systemMainColor.opacity(回答.isEmpty ? 0.7 : 1.0))
                         Text("送出")
-                        .font(.custom("LXGWWenKaiMonoTC-Regular", size: 16))
-                        .foregroundColor(.white)
+                            .font(.custom("LXGWWenKaiMonoTC-Regular", size: 16))
+                            .foregroundColor(.white)
                     }
                 }
                 .frame(width: 60, height: 28)
